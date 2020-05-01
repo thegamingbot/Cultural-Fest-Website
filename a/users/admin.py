b6513dfe17e53d6dcb6f8caa52a52979144735bd
Sai Kaushik S <saikaushik609@gmail.com>
2020-05-01 18:56:16 +0530

    11

--- a/users/admin.py
+++ a/users/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+from .models import Events, SelectedEvents
 
 from .models import Events
 # Register your models here.
 admin.site.register(Events)
+admin.site.register(SelectedEvents)
--- a/users/models.py
+++ a/users/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Events(models.Model):
@@ -8,3 +9,8 @@ class Events(models.Model):
 
     def __str__(self):
         return f"ID: {self.ID}   Name: {self.Name}   Cost: {self.Cost}"
+
+class SelectedEvents(models.Model):
+    Name = models.ForeignKey(User, on_delete=models.CASCADE)
+    Events = models.ManyToManyField(Events, blank=True)
+
