b40b5c9a72a5cd30e421121d1df61336d960a0a3
nikunj200 <47524194+nikunj200@users.noreply.github.com>
2020-05-11 23:24:25 +0530

    basic chatroom

--- a/a/settings.py
+++ a/a/settings.py
@@ -73,7 +73,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'a.wsgi.application'
 ASGI_APPLICATION = 'a.routing.application'
-
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
